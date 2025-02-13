5e7593cf081c268efafd6e3e19629e45d879a3ad
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-02-12 17:15:37 -0800

    Create patient_support_analysis_1.sql

---
+++ Easy/Patient Support Analysis (Part 1)/patient_support_analysis_1.sql
@@ -0,0 +1,15 @@
+
+      
+SELECT 
+    COUNT(policy_holder_id) AS policy_holder_count
+FROM 
+    (
+  SELECT 
+      policy_holder_id 
+  FROM
+      callers
+  GROUP BY 
+      policy_holder_id
+  HAVING
+      COUNT(case_id) >=3
+) AS call_records;
